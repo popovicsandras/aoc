9b8461c495d6c99ac2ba8ebaf5f2936d9700fe71
Andras Popovics <popovics@ndras.hu>
2022-12-03 13:59:57 +0100

    Day 3/1

--- packages/2022/src/lib/day-3/index.ts
+++ packages/2022/src/lib/day-3/index.ts
@@ -1,12 +1,37 @@
 import { resolve } from 'path';
 import readLinesFromFile from '../utils/file-reader';
 
+const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const priorities: any = alpha
+  .map((x) => String.fromCharCode(x))
+  .reduce((acc, value, index) => {
+    return {
+      ...acc,
+      [value.toLowerCase()]: index + 1,
+      [value]: index + 27
+    }
+  }, {});
+
 const main = async () => {
   const fileReader = readLinesFromFile(resolve(__dirname, 'input.txt'));
 
+  let sum = 0;
   for await (let line of fileReader) {
-    console.log(line);
+    let buffer = new Set();
+
+    for (let i = 0; i < (line.length-1) / 2; i++) {
+      buffer.add(line[i]);
+    }
+
+    for (let i = (line.length) / 2; i < line.length; i++) {
+      if (buffer.has(line[i])) {
+        sum += priorities[line[i]];
+        break;
+      }
+    }
   }
+
+  console.log(sum);
 }
 
 main();
