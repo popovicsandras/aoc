3fab10f6e22644248ab57cb6d9788c03d0cc9cc7
Andras Popovics <popovics@ndras.hu>
2022-12-03 16:05:54 +0100

    Day 3/2

--- packages/2022/src/lib/day-3/index.ts
+++ packages/2022/src/lib/day-3/index.ts
@@ -12,26 +12,51 @@ const priorities: any = alpha
     }
   }, {});
 
+const findBadgeValue = (uniquItemsInSacks: any[], lineNumber: number) => {
+  const elf1Sack: any = '0b' + uniquItemsInSacks[lineNumber-3].join('');
+  const elf2Sack: any = '0b' + uniquItemsInSacks[lineNumber-2].join('');
+  const elf3Sack: any = '0b' + uniquItemsInSacks[lineNumber-1].join('');
+  const index: any = BigInt(elf1Sack) & BigInt(elf2Sack) & BigInt(elf3Sack);
+  return alpha.length*2 - BigInt(index).toString(2).length + 1
+}
+
 const main = async () => {
   const fileReader = readLinesFromFile(resolve(__dirname, 'input.txt'));
 
   let sum = 0;
+  let badgeSum = 0;
+  let lineNumber = 0
+  let uniquItemsInSacks: any = [];
+
   for await (let line of fileReader) {
+    uniquItemsInSacks[lineNumber] = Array(alpha.length*2).fill(0);
     let buffer = new Set();
 
     for (let i = 0; i < (line.length-1) / 2; i++) {
+      uniquItemsInSacks[lineNumber][priorities[line[i]]-1] = 1;
       buffer.add(line[i]);
     }
 
     for (let i = (line.length) / 2; i < line.length; i++) {
+      uniquItemsInSacks[lineNumber][priorities[line[i]]-1] = 1;
+
       if (buffer.has(line[i])) {
         sum += priorities[line[i]];
-        break;
+        // break; // Makes sense only for the first part for perf optimisation
       }
     }
+
+    if ((lineNumber % 3 === 0) && (lineNumber > 0)) {
+      badgeSum += findBadgeValue(uniquItemsInSacks, lineNumber);
+    }
+
+    lineNumber++;
   }
 
-  console.log(sum);
+  badgeSum += findBadgeValue(uniquItemsInSacks, lineNumber);
+
+  console.log('Part 1:', sum);
+  console.log('Part 2:', badgeSum);
 }
 
 main();
