bb9da58c8fb6bd54638bf45b6cfb5d1cc75e8ac1
Andras Popovics <popovics@ndras.hu>
2022-12-04 22:22:38 +0100

    Add basePath

--- apps/webapp/next.config.js
+++ apps/webapp/next.config.js
@@ -3,10 +3,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
+console.log(process.env.LOCAL_SERVE)
+
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  ...(process.env.LOCAL_SERVE ? {} : { basePath: '/aoc' }),
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
